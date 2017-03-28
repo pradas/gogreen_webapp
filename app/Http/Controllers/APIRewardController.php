fe6abb3d4e8f24b03e200a367ee39e7012edbae6
Gerard Pradas <gerard.pradas.san.jose@est.fib.upc.edu>
2017-03-28 17:45:05 +0200

    hotfix api

--- app/Http/Controllers/APIRewardController.php
+++ app/Http/Controllers/APIRewardController.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class APIRewardController extends Controller
 {
     public function index() {
-        return Reward::all();
+        return "{ \"rewards\": " . Reward::all() . "}";
     }
 }

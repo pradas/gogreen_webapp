aafd24e5df7230ce67ea7b6f6f245e688c3bcf3c
Gerard Pradas <gerard.pradas.san.jose@est.fib.upc.edu>
2017-04-19 18:19:50 +0200

    fixed rewards of user

--- app/RewardUser.php
+++ app/RewardUser.php
@@ -2,12 +2,33 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class RewardUser extends Model
 {
     protected $table = 'reward_user';
-    protected $appends = ['category'];
+
+    protected $hidden = ['user_id', 'reward_id', 'created_at', 'updated_at'];
+    protected $appends = ['title', 'points', 'category', 'end_date', 'exchange_date'];
+
+
+    public function getTitleAttribute() {
+        return Reward::find($this->attributes['reward_id'])->title;
+    }
+    public function getPointsAttribute() {
+        return Reward::find($this->attributes['reward_id'])->points;
+    }
+    public function getCategoryAttribute() {
+        return Reward::find($this->attributes['reward_id'])->category;
+    }
+    public function getEndDateAttribute() {
+        return Reward::find($this->attributes['reward_id'])->end_date;
+    }
+    public function getExchangeDateAttribute() {
+        return Reward::find($this->attributes['reward_id'])->exchange_date;
+    }
+
 
     public function users()
     {

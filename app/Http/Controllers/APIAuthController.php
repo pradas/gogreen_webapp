6503e842c8f20ddaa991ff3f20621a794027316c
Gerard Pradas <gerard.pradas.san.jose@est.fib.upc.edu>
2017-06-09 16:13:18 +0200

    added 1000 points to total points

--- app/Http/Controllers/APIAuthController.php
+++ app/Http/Controllers/APIAuthController.php
@@ -22,6 +22,7 @@ class APIAuthController extends Controller
             $user->password = bcrypt($request->password);
             $user->role_id = '4';
             $user->points = 1000;
+            $user->total_points = 1000;
             $user->image = self::DEFAULT_USER_IMAGE;
             $user->save();
         } catch (\Exception $e) {
